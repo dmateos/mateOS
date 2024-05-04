8404257a23c29567686a7a23427de4bdb68b81b7
Daniel Mateos <daniel@mateos.cc>
2024-05-05 01:02:45 +1000

    add kernel.h

--- src/kernel.c
+++ src/kernel.c
@@ -1,19 +1,6 @@
 #include "kernel.h"
 #include "arch/i686/legacytty.h"
 
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
-
-#if defined(__linux__)
-#error                                                                         \
-    "You are not using a cross-compiler, you will most certainly run into trouble"
-#endif
-
-#if !defined(__i386__)
-#error "i386 required"
-#endif
-
 void kernel_main(void) {
   terminal_initialize();
 
