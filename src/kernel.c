85e8cd6473b2eec132489fd815eeae8c37416601
Daniel Mateos <daniel@mateos.cc>
2024-05-04 23:56:06 +1000

    newline

--- src/kernel.c
+++ src/kernel.c
@@ -14,7 +14,8 @@
 #endif
 
 void kernel_main(void) {
-  /* Initialize terminal interface */
   terminal_initialize();
-  terminal_writestring("Hello, kernel World!\n");
+  while (1) {
+    terminal_writestring("Hello, kernel World!\n");
+  }
 }
