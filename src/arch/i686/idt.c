5fcda1629f46b38a72149c396fb6cded6865a2ad
Daniel Mateos <daniel@mateos.cc>
2024-05-06 17:24:09 +1000

    refactors

--- src/arch/i686/idt.c
+++ src/arch/i686/idt.c
@@ -1,5 +1,5 @@
 #include "idt.h"
-#include "legacytty.h"
+#include "../../lib.h"
 
 idt_entry_t idt_entries[256];
 idt_ptr_t idt_ptr;
@@ -8,8 +8,8 @@ void init_idt() {
   idt_ptr.limit = sizeof(idt_entry_t) * 256 - 1;
   idt_ptr.base = (uint32_t)&idt_entries;
 
-  term_writestr("IDT initializing\n");
-  term_writestr("IDT not implemented :(\n");
+  printf("IDT initializing\n");
+  printf("IDT not implemented :(\n");
 
   // memset(&idt_entries, 0, sizeof(idt_entry_t) * 256);
   // idt_load(&idt_ptr);
