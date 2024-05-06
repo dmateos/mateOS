5be9a78a1ff5c1e1012db6c3ef195726d3d26b8f
Daniel Mateos <daniel@mateos.cc>
2024-05-06 23:15:43 +1000

    add utils for printing registers/stack etc

---
+++ src/arch/i686/util.c
@@ -0,0 +1,27 @@
+#include "util.h"
+#include "../../lib.h"
+
+void print_registers(void) {
+  uint32_t eax, ebx, ecx, edx;
+
+  asm volatile("mov %%eax, %0" : "=r"(eax));
+  asm volatile("mov %%ebx, %0" : "=r"(ebx));
+  asm volatile("mov %%ecx, %0" : "=r"(ecx));
+  asm volatile("mov %%edx, %0" : "=r"(edx));
+
+  printf("EAX: 0x%x\n", eax);
+  printf("EBX: 0x%x\n", ebx);
+  printf("ECX: 0x%x\n", ecx);
+  printf("EDX: 0x%x\n", edx);
+}
+
+void print_stack(int entries) {
+  uint32_t *ebp;
+
+  asm volatile("mov %%ebp, %0" : "=r"(ebp));
+
+  printf("Stack trace:\n");
+  for (int i = 0; i < entries; i++) {
+    printf("0x%x\n", ebp[i]);
+  }
+}
---
+++ src/arch/i686/util.h
@@ -0,0 +1,7 @@
+#ifndef _UTIL_H
+#define _UTIL_H
+
+void print_registers(void);
+void print_stack(int entries);
+
+#endif
