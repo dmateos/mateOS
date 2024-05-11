83bea4cd97b0fd099ffb590851921bdbdbbe3135
Daniel Mateos <daniel@mateos.cc>
2024-05-11 20:12:06 +1000

    refactors

--- src/arch/i686/interrupts.c
+++ src/arch/i686/interrupts.c
@@ -8,6 +8,9 @@
 #define SLAVE_PIC_COMMAND 0xA0
 #define SLAVE_PIC_DATA 0xA1
 
+#define SEGMENT_OFFSET 0x08
+#define PRIVILEGE 0x8E
+
 static void pic_remap(void) {
   // Remap the PIC so we can use interrupts
   outb(MASTER_PIC_COMMAND, 0x11); // Start initialization sequence
@@ -51,60 +54,59 @@ static void write_idt_entry(idt_entry_t *idt_entries, uint8_t num,
 }
 
 static void init_idt_table(idt_entry_t *ide) {
-  int segment = 0x08;
   // This is a dumb ass way to do this but i CBF writting a
   // lookup table in assembly.
-  // is the segment offset correct here? i dont know.
-  write_idt_entry(ide, 0, (uint32_t)isr0, segment, 0x8E);
-  write_idt_entry(ide, 1, (uint32_t)isr1, segment, 0x8E);
-  write_idt_entry(ide, 2, (uint32_t)isr2, segment, 0x8E);
-  write_idt_entry(ide, 3, (uint32_t)isr3, segment, 0x8E);
-  write_idt_entry(ide, 4, (uint32_t)isr4, segment, 0x8E);
-  write_idt_entry(ide, 5, (uint32_t)isr5, segment, 0x8E);
-  write_idt_entry(ide, 6, (uint32_t)isr6, segment, 0x8E);
-  write_idt_entry(ide, 7, (uint32_t)isr7, segment, 0x8E);
-  write_idt_entry(ide, 8, (uint32_t)isr8, segment, 0x8E);
-  write_idt_entry(ide, 9, (uint32_t)isr9, segment, 0x8E);
-  write_idt_entry(ide, 10, (uint32_t)isr10, segment, 0x8E);
-  write_idt_entry(ide, 11, (uint32_t)isr11, segment, 0x8E);
-  write_idt_entry(ide, 12, (uint32_t)isr12, segment, 0x8E);
-  write_idt_entry(ide, 13, (uint32_t)isr13, segment, 0x8E);
-  write_idt_entry(ide, 14, (uint32_t)isr14, segment, 0x8E);
-  write_idt_entry(ide, 15, (uint32_t)isr15, segment, 0x8E);
-  write_idt_entry(ide, 16, (uint32_t)isr16, segment, 0x8E);
-  write_idt_entry(ide, 17, (uint32_t)isr17, segment, 0x8E);
-  write_idt_entry(ide, 18, (uint32_t)isr18, segment, 0x8E);
-  write_idt_entry(ide, 19, (uint32_t)isr19, segment, 0x8E);
-  write_idt_entry(ide, 20, (uint32_t)isr20, segment, 0x8E);
-  write_idt_entry(ide, 21, (uint32_t)isr21, segment, 0x8E);
-  write_idt_entry(ide, 22, (uint32_t)isr22, segment, 0x8E);
-  write_idt_entry(ide, 23, (uint32_t)isr23, segment, 0x8E);
-  write_idt_entry(ide, 24, (uint32_t)isr24, segment, 0x8E);
-  write_idt_entry(ide, 25, (uint32_t)isr25, segment, 0x8E);
-  write_idt_entry(ide, 26, (uint32_t)isr26, segment, 0x8E);
-  write_idt_entry(ide, 27, (uint32_t)isr27, segment, 0x8E);
-  write_idt_entry(ide, 28, (uint32_t)isr28, segment, 0x8E);
-  write_idt_entry(ide, 29, (uint32_t)isr29, segment, 0x8E);
-  write_idt_entry(ide, 30, (uint32_t)isr30, segment, 0x8E);
-  write_idt_entry(ide, 31, (uint32_t)isr31, segment, 0x8E);
+  // is the SEGMENT_OFFSET offset correct here? i dont know.
+  write_idt_entry(ide, 0, (uint32_t)isr0, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 1, (uint32_t)isr1, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 2, (uint32_t)isr2, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 3, (uint32_t)isr3, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 4, (uint32_t)isr4, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 5, (uint32_t)isr5, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 6, (uint32_t)isr6, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 7, (uint32_t)isr7, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 8, (uint32_t)isr8, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 9, (uint32_t)isr9, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 10, (uint32_t)isr10, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 11, (uint32_t)isr11, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 12, (uint32_t)isr12, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 13, (uint32_t)isr13, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 14, (uint32_t)isr14, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 15, (uint32_t)isr15, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 16, (uint32_t)isr16, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 17, (uint32_t)isr17, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 18, (uint32_t)isr18, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 19, (uint32_t)isr19, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 20, (uint32_t)isr20, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 21, (uint32_t)isr21, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 22, (uint32_t)isr22, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 23, (uint32_t)isr23, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 24, (uint32_t)isr24, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 25, (uint32_t)isr25, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 26, (uint32_t)isr26, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 27, (uint32_t)isr27, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 28, (uint32_t)isr28, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 29, (uint32_t)isr29, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 30, (uint32_t)isr30, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 31, (uint32_t)isr31, SEGMENT_OFFSET, PRIVILEGE);
 
   // IRQs
-  write_idt_entry(ide, 32, (uint32_t)irq0, segment, 0x8E);
-  write_idt_entry(ide, 33, (uint32_t)irq1, segment, 0x8E);
-  write_idt_entry(ide, 34, (uint32_t)irq2, segment, 0x8E);
-  write_idt_entry(ide, 35, (uint32_t)irq3, segment, 0x8E);
-  write_idt_entry(ide, 36, (uint32_t)irq4, segment, 0x8E);
-  write_idt_entry(ide, 37, (uint32_t)irq5, segment, 0x8E);
-  write_idt_entry(ide, 38, (uint32_t)irq6, segment, 0x8E);
-  write_idt_entry(ide, 39, (uint32_t)irq7, segment, 0x8E);
-  write_idt_entry(ide, 40, (uint32_t)irq8, segment, 0x8E);
-  write_idt_entry(ide, 41, (uint32_t)irq9, segment, 0x8E);
-  write_idt_entry(ide, 42, (uint32_t)irq10, segment, 0x8E);
-  write_idt_entry(ide, 43, (uint32_t)irq11, segment, 0x8E);
-  write_idt_entry(ide, 44, (uint32_t)irq12, segment, 0x8E);
-  write_idt_entry(ide, 45, (uint32_t)irq13, segment, 0x8E);
-  write_idt_entry(ide, 46, (uint32_t)irq14, segment, 0x8E);
-  write_idt_entry(ide, 47, (uint32_t)irq15, segment, 0x8E);
+  write_idt_entry(ide, 32, (uint32_t)irq0, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 33, (uint32_t)irq1, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 34, (uint32_t)irq2, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 35, (uint32_t)irq3, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 36, (uint32_t)irq4, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 37, (uint32_t)irq5, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 38, (uint32_t)irq6, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 39, (uint32_t)irq7, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 40, (uint32_t)irq8, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 41, (uint32_t)irq9, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 42, (uint32_t)irq10, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 43, (uint32_t)irq11, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 44, (uint32_t)irq12, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 45, (uint32_t)irq13, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 46, (uint32_t)irq14, SEGMENT_OFFSET, PRIVILEGE);
+  write_idt_entry(ide, 47, (uint32_t)irq15, SEGMENT_OFFSET, PRIVILEGE);
 }
 
 void init_idt(idt_ptr_t *idt_ptr, idt_entry_t *idt_entries) {
