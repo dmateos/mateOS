fd2184fdbbd48e864aa14f8af1add2f2225dc4ac
Daniel Mateos <daniel@mateos.cc>
2024-08-18 23:29:00 +1000

    work on assembler port

--- src/interprust/src/main.rs
+++ src/interprust/src/main.rs
@@ -1,5 +1,9 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Read};
+
 enum Instructions {
-    ADD,
+    ADD = 0x01,
     SUB,
     PRINT,
     JMP,
@@ -45,9 +49,34 @@ impl VirtualMachine {
     }
 }
 
-fn assemble_file(filename: &str) {}
+fn assemble_file(filename: &str) {
+    let file = File::open(filename).expect("file not found");
+    let mut new_file = File::create("output").expect("could not create file");
+    let labels: HashMap<&str, i32> = HashMap::new();
+
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let mut parts = line.split(" ").collect::<Vec<&str>>();
+
+        'stop: loop {
+            if parts.len() == 0 {
+                break 'stop;
+            }
+
+            let token = parts.pop().unwrap();
+            match token {
+                "ADD" => {}
+                _ => {
+                    println!("Unknown instruction: {}", token);
+                }
+            }
+        }
+    }
+}
 
 fn main() {
+    assemble_file("test.s");
     let mut vm = VirtualMachine::new();
-    vm.run_vm();
+    //vm.run_vm();
 }
