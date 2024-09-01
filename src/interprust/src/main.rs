7785967f33f4257c07b7b4738ba86b625bd85390
Daniel Mateos <daniel@mateos.cc>
2024-09-01 16:43:37 +1000

    work on vm

--- src/interprust/src/main.rs
+++ src/interprust/src/main.rs
@@ -25,64 +25,81 @@ struct VirtualMachine {
 }
 
 impl VirtualMachine {
-    fn new(filename: &str, offset: i32) -> VirtualMachine {
-        let file = File::open(filename).expect("file not found");
-
-        return VirtualMachine {
+    fn new(filename: &str, offset: usize) -> VirtualMachine {
+        let mut vm = VirtualMachine {
             text: Vec::new(),
             stack: Vec::new(),
             reg0: 0,
-            ip: 0,
+            ip: offset,
             sp: 0,
             sbp: 0,
         };
+
+        let file = File::open(filename).expect("file not found");
+        let mut reader = BufReader::new(file);
+        let mut buffer = [0u8; 4];
+
+        while reader.read_exact(&mut buffer).is_ok() {
+            let value = u32::from_le_bytes(buffer);
+            vm.text.push(value);
+        }
+
+        return vm;
     }
 
     fn print_state(&self) {
         println!("VM State");
-        println!("{}", self.reg0);
-        println!("{}", self.ip);
-        println!("{}", self.sp);
-        println!("{}", self.sbp);
+        println!("reg0: {}", self.reg0);
+        println!("ip: {}", self.ip);
+        println!("sp: {}", self.sp);
+        println!("sbp: {}", self.sbp);
     }
 
     fn run_vm(&mut self) {
         println!("Running:");
         loop {
             match self.text[self.ip] {
+                //ADD
                 0x1 => {
                     self.ip += 1;
                     self.reg0 += self.text[self.ip];
                     self.ip += 1;
                 }
+                //SUB
                 0x02 => {
                     self.ip += 1;
                     self.reg0 -= self.text[self.ip];
                     self.ip += 1;
                 }
+                //PRINT
                 0x03 => {
                     self.ip += 1;
                     println!("{}", self.reg0);
                 }
+                //JMP
                 0x04 => {
                     self.ip += 1;
                     self.ip = self.text[self.ip] as usize;
                 }
+                //PUSH
                 0x05 => {
                     self.ip += 1;
                     self.sp += 1;
                     self.stack.push(self.reg0);
                 }
+                //POP
                 0x06 => {
                     self.ip += 1;
                     self.sp -= 1;
                     self.reg0 = self.stack.pop().unwrap();
                 }
+                //SET
                 0x07 => {
                     self.ip += 1;
                     self.reg0 = self.text[self.ip];
                     self.ip += 1;
                 }
+                //CALL
                 0x08 => {
                     self.ip += 1;
                     self.sbp = self.sp;
@@ -90,6 +107,7 @@ impl VirtualMachine {
                     self.sp += 1;
                     self.ip = self.text[self.ip] as usize;
                 }
+                //RET
                 0x09 => {
                     self.sp -= 1;
                     self.ip = self.stack[self.sp] as usize;
@@ -129,7 +147,7 @@ fn assemble_file(filename: &str) {
 
 fn main() {
     //assemble_file("test.s");
-    let mut vm = VirtualMachine::new("output", 100);
-    vm.run_vm();
+    let mut vm = VirtualMachine::new("output", 12);
     vm.print_state();
+    vm.run_vm();
 }
