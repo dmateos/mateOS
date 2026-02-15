b7599c1f34142f079f84bef9b4db46d340bc0ce3
Daniel Mateos <daniel@mateos.cc>
2026-02-15 15:03:24 +1100

    cpu usage test app

---
+++ userland/burn.c
@@ -0,0 +1,17 @@
+#include "syscalls.h"
+#include "libc.h"
+
+void _start(int argc, char **argv) {
+    (void)argc;
+    (void)argv;
+
+    print("burn: busy loop started (Ctrl+C/kill to stop)\n");
+
+    volatile unsigned int x = 0x12345678u;
+    while (1) {
+        // Keep ALU and branch units active so scheduler sees sustained CPU use.
+        x ^= (x << 13);
+        x ^= (x >> 17);
+        x ^= (x << 5);
+    }
+}
